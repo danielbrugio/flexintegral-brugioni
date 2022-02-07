29ce5072efe53e2a85e91293dc0997027e426753
Daniel Brugioni <65047232+danielbrugio@users.noreply.github.com>
2022-02-07 13:32:09 -0300

    Navbar changes

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,24 +1,5 @@
-/* import "./NavBar.css";
-import Logo from "../assets/Logo.png";
-
-const NavBar = () => {
-  return (
-    <nav className="navBar">
-      <img src={Logo} alt="" />
-      <div className="categories">
-        <button className="option">Notebooks</button>
-        <button className="option">Smartphones</button>
-        <button className="option">Tablets</button>
-        <button className="option">TV</button>
-      </div>
-    </nav>
-  );
-};
-
-export default NavBar; */
 import "./NavBar.css";
-
-const { Navbar, Container, Nav } = require("react-bootstrap");
+import { Navbar, Container, Nav } from "react-bootstrap";
 import Logo from "../assets/Logo.png";
 
 const NavBar = () => {
