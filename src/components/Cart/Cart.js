44c7a30a00d8328d9e483417a13e1847b9b01818
danielbrugio <daniel.brugioni@hotmail.com>
2022-03-15 21:11:18 -0300

    firebase added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -22,7 +22,7 @@ const Cart = () => {
             {
                 products.map(prod => {
                     return (
-                        <div key={prod.id} style={{ display: 'flex'}}>
+                        <div key={prod.name} style={{ display: 'flex'}}>
                             <h3>{prod.name}</h3>
                             <h3>Cantidad {prod.quantity}</h3>
                             <button onClick={() => handleRemoveItem(prod.id, prod.name)}>X</button>
