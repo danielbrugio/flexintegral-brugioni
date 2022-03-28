64c9eeeb50715a286b9e89a94fbcdcee1e955b19
danielbrugio <daniel.brugioni@hotmail.com>
2022-03-27 21:32:21 -0300

    entrega final

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getDocs, collection, query, where, getDoc, doc } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyABwj1GebR_MRRvfL6TVcjEt_prjXEnn-Q",
@@ -13,3 +14,34 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const firestoreDb = getFirestore(app)
+
+export const getProducts = (categoryId) => {
+  return new Promise((resolve, reject) => {
+    const collectionRef = categoryId ?
+      query(collection(firestoreDb, 'products'), where('category', '==', categoryId)) :
+      collection(firestoreDb, 'products')
+
+    getDocs(collectionRef).then(response => {
+        const products = response.docs.map(doc => {
+            return { id: doc.id, ...doc.data() }
+        })
+
+        resolve(products)
+    }).catch((error) => {
+        reject('No products found: ', error)
+    })
+  })
+}
+
+export const getProductById = (productId) => {
+  return new Promise((resolve ,reject) => {
+    const docRef = doc(firestoreDb, 'products', productId)
+
+    getDoc(docRef).then(response => {
+        const product = { id: response.id, ...response.data()}
+        resolve(product)
+    }).catch((error) => {
+        reject('No products found: ', error)
+    })
+  })
+}
