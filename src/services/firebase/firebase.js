bbca3dca86f80ac7d70659b08b39af1d6ae2a777
danielbrugio <daniel.brugioni@hotmail.com>
2022-03-28 22:11:12 -0300

    entrega final

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -1,9 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getDocs, collection, query, where, getDoc, doc } from 'firebase/firestore'
+import {
+  getDocs,
+  collection,
+  query,
+  where,
+  getDoc,
+  doc,
+} from "firebase/firestore";
 
 const firebaseConfig = {
- /*  apiKey: process.env.REACT_APP_apiKey,
+  /*  apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
   projectId: process.env.REACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
@@ -14,40 +21,47 @@ const firebaseConfig = {
   projectId: "react-dani",
   storageBucket: "react-dani.appspot.com",
   messagingSenderId: "1043824621430",
-  appId: "1:1043824621430:web:7c46ebb52db67c3db5ac57"
+  appId: "1:1043824621430:web:7c46ebb52db67c3db5ac57",
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const firestoreDb = getFirestore(app)
+export const firestoreDb = getFirestore(app);
 
 export const getProducts = (categoryId) => {
   return new Promise((resolve, reject) => {
-    const collectionRef = categoryId ?
-      query(collection(firestoreDb, 'products'), where('category', '==', categoryId)) :
-      collection(firestoreDb, 'products')
+    const collectionRef = categoryId
+      ? query(
+          collection(firestoreDb, "products"),
+          where("category", "==", categoryId)
+        )
+      : collection(firestoreDb, "products");
 
-    getDocs(collectionRef).then(response => {
-        const products = response.docs.map(doc => {
-            return { id: doc.id, ...doc.data() }
-        })
+    getDocs(collectionRef)
+      .then((response) => {
+        const products = response.docs.map((doc) => {
+          return { id: doc.id, ...doc.data() };
+        });
 
-        resolve(products)
-    }).catch((error) => {
-        reject('No products found: ', error)
-    })
-  })
-}
+        resolve(products);
+      })
+      .catch((error) => {
+        reject("No products found: ", error);
+      });
+  });
+};
 
 export const getProductById = (productId) => {
-  return new Promise((resolve ,reject) => {
-    const docRef = doc(firestoreDb, 'products', productId)
+  return new Promise((resolve, reject) => {
+    const docRef = doc(firestoreDb, "products", productId);
 
-    getDoc(docRef).then(response => {
-        const product = { id: response.id, ...response.data()}
-        resolve(product)
-    }).catch((error) => {
-        reject('No products found: ', error)
-    })
-  })
-}
+    getDoc(docRef)
+      .then((response) => {
+        const product = { id: response.id, ...response.data() };
+        resolve(product);
+      })
+      .catch((error) => {
+        reject("No products found: ", error);
+      });
+  });
+};
