a487e84e5ba048db3f3abdb424a214b559c59970
danielbrugio <daniel.brugioni@hotmail.com>
2022-03-29 19:27:49 -0300

    entrega final

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -10,18 +10,12 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  /*  apiKey: process.env.REACT_APP_apiKey,
+   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
   projectId: process.env.REACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId */
-  apiKey: "AIzaSyABwj1GebR_MRRvfL6TVcjEt_prjXEnn-Q",
-  authDomain: "react-dani.firebaseapp.com",
-  projectId: "react-dani",
-  storageBucket: "react-dani.appspot.com",
-  messagingSenderId: "1043824621430",
-  appId: "1:1043824621430:web:7c46ebb52db67c3db5ac57",
+  appId: process.env.REACT_APP_appId
 };
 
 const app = initializeApp(firebaseConfig);
